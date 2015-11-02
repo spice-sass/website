08999902f04d11c7af08ed97a2c56afe1e37d6c8
dan osborne <danosborne@rocketmail.com>
2015-11-02 23:23:12 +0000

    updated homepage styles

--- app/components/docs/List.js
+++ app/components/docs/List.js
@@ -6,23 +6,6 @@ var List = React.createClass({
 		var inc = this.props.includes;
 		return (
 			<div className="scrollbar" id="docs">
-				<div className="include-block" id="getting-started">
-					<h1>Getting Started</h1>
-					<hr />
-
-					<div id="installation">
-						<h2 className="red">Installation</h2>
-						<p>{'Spice is available for download via npm or bower'}</p>
-						<h5>npm</h5>
-						<blockquote>npm install spice-sass</blockquote>
-						<h5>Bower</h5>
-						<blockquote>bower install spice-sass</blockquote>
-					</div>
-
-					<hr />
-					
-				</div>
-
 				{this.props.order.map(function(ord){
 					return <MixinGroup ord={ord} includes={inc}/>
 				})}
--- app/components/docs/Sidebar.js
+++ app/components/docs/Sidebar.js
@@ -11,16 +11,6 @@ var Sidebar = React.createClass({
 		return (
 			<nav className="page-nav scrollbar" id="sidebar">
 				<ul className="vertical-nav">
-					<li>
-						<a onClick={goToMixin.bind(this,'getting-started')}>Getting Started</a>
-						<ul className="subnav">
-							<li>
-								<a onClick={goToMixin.bind(this,'installation')}>Installation</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-				<ul className="vertical-nav">
 					{this.props.order.map(function(ord){
 						return <SBLink ord={ord} includes={inc} goToMixin={goToMixin}/>
 					})}
