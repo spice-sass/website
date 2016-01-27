a863392b0d36ad99b755e32906a8f438ffafc672
Dan Osborne <danosborne@rocketmail.com>
2016-01-27 17:41:45 +0000

    various changes

--- build/js/saffron.js
+++ build/js/saffron.js
@@ -1,1 +1,93 @@
-!function e(t,s,r){function a(i,c){if(!s[i]){if(!t[i]){var l="function"==typeof require&&require;if(!c&&l)return l(i,!0);if(n)return n(i,!0);throw new Error("Cannot find module '"+i+"'")}var u=s[i]={exports:{}};t[i][0].call(u.exports,function(e){var s=t[i][1][e];return a(s?s:e)},u,u.exports,e,t,s,r)}return s[i].exports}for(var n="function"==typeof require&&require,i=0;i<r.length;i++)a(r[i]);return a}({1:[function(e,t,s){"use strict";var r=React.createClass({displayName:"Saffron",getInitialState:function(){return{slideAway:!1,success:!1,error:!1,msg:"",user:""}},handleClick:function(){var e=this.refs.textInput.getDOMNode().value,t=(this.refs.passInput.getDOMNode().value,this.success.bind(this));e.length<5||e.length<5?this.setState({error:!0,msg:"Username not recognised"}):(this.setState({slideAway:!0}),setTimeout(function(){t(e)},700))},success:function(e){this.setState({success:!0,error:!1,msg:"",user:e})},render:function(){return React.createElement("div",null,0==this.state.success?React.createElement("form",{className:this.state.slideAway?"lined slide-away":"lined"},React.createElement("label",{className:this.state.error?"slide-in-up error":"slide-in-up"},"Username"),React.createElement("input",{className:"slide-in-up",type:"text",ref:"textInput"}),React.createElement("label",{className:this.state.error?"slide-in-up error":"slide-in-up"},"Password"),React.createElement("input",{className:"slide-in-up",type:"password",ref:"passInput"}),React.createElement("button",{type:"button",onClick:this.handleClick,className:"flat-submit block-icon-check slide-in-up"},"Login"),this.state.error&&React.createElement("p",{className:"error-msg slide-in-up"},this.state.msg)):React.createElement("div",{className:"success-message fade-in"},React.createElement("i",{className:"line-icon-head"}),React.createElement("h5",null,"Welcome back ",this.state.user)))}});React.render(React.createElement(r,null),document.getElementById("saffron-form"))},{}]},{},[1]);
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+var Saffron = React.createClass({
+	displayName: "Saffron",
+
+	getInitialState: function getInitialState() {
+		return {
+			slideAway: false,
+			success: false,
+			error: false,
+			msg: "",
+			user: ""
+		};
+	},
+
+	handleClick: function handleClick() {
+
+		var textVal = this.refs.textInput.getDOMNode().value,
+		    passVal = this.refs.passInput.getDOMNode().value,
+		    success = this.success.bind(this);
+
+		if (textVal.length < 5 || textVal.length < 5) {
+			this.setState({
+				error: true,
+				msg: "Username not recognised"
+			});
+		} else {
+			this.setState({
+				slideAway: true
+			});
+			setTimeout(function () {
+				success(textVal);
+			}, 700);
+		}
+	},
+
+	success: function success(user) {
+		this.setState({
+			success: true,
+			error: false,
+			msg: "",
+			user: user
+		});
+	},
+
+	render: function render() {
+		return React.createElement(
+			"div",
+			null,
+			this.state.success == false ? React.createElement(
+				"form",
+				{ className: this.state.slideAway ? "lined slide-away" : "lined" },
+				React.createElement(
+					"label",
+					{ className: this.state.error ? 'slide-in-up error' : 'slide-in-up' },
+					"Username"
+				),
+				React.createElement("input", { className: "slide-in-up", type: "text", ref: "textInput" }),
+				React.createElement(
+					"label",
+					{ className: this.state.error ? 'slide-in-up error' : 'slide-in-up' },
+					'Password'
+				),
+				React.createElement("input", { className: "slide-in-up", type: "password", ref: "passInput" }),
+				React.createElement(
+					"button",
+					{ type: "button", onClick: this.handleClick, className: "flat-submit block-icon-check slide-in-up" },
+					"Login"
+				),
+				this.state.error && React.createElement(
+					"p",
+					{ className: "error-msg slide-in-up" },
+					this.state.msg
+				)
+			) : React.createElement(
+				"div",
+				{ className: "success-message fade-in" },
+				React.createElement("i", { className: "line-icon-head" }),
+				React.createElement(
+					"h5",
+					null,
+					"Welcome back ",
+					this.state.user
+				)
+			)
+		);
+	}
+});
+
+React.render(React.createElement(Saffron, null), document.getElementById('saffron-form'));
+
+},{}]},{},[1])
